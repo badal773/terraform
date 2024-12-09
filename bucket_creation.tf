482576ee45677cfab358f4070fb1dd940d371fe6
Badal Kumar <130441461+badal773@users.noreply.github.com>
2024-12-09 16:49:30 +0530

    Update bucket_creation.tf

--- bucket_creation.tf
+++ bucket_creation.tf
@@ -8,8 +8,8 @@ resource "aws_s3_bucket" "example" {
 }
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "$key"
-  secret_key = "$key"
-  token = "$key"
+  access_key = "$AWS_ACCESS_KEY_ID"
+  secret_key = "$AWS_SECRET_ACCESS_KEY"
+  token = "$AWS_SESSION_TOKEN"
 
 }
