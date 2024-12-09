0111ad193f59f369cf5c0890a5751a9b5dab2221
Badal Kumar <130441461+badal773@users.noreply.github.com>
2024-12-10 00:00:51 +0530

    Update bucket_creation.tf

--- bucket_creation.tf
+++ bucket_creation.tf
@@ -1,19 +1,4 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "bucket-create-from-tofu-controller"
 
-  tags = {
-    Name        = "badal773"
-    Environment = "Dev"
-  }
-}
-resource "aws_s3_bucket" "example-02" {
-  bucket = "bucket-create-from-tofu-controller-02"
-
-  tags = {
-    Name        = "badal773"
-    Environment = "Dev"
-  }
-}
 
 provider "aws" {
   region     = "ap-south-1"
