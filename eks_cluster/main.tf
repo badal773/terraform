3c9166f2424fc4654b21d7342aafd468756b153e
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2024-12-10 14:23:03 +0530

    added new main.tf

--- eks_cluster/main.tf
+++ eks_cluster/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = local.region
+}
 terraform {
   required_version = ">= 1.3.2"
 
@@ -6,24 +9,11 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.79"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9"
-    }
   }
 }
 
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Filter out local zones, which are not currently supported 
-# with managed node groups
 data "aws_availability_zones" "available" {
+  # Exclude local zones
   filter {
     name   = "opt-in-status"
     values = ["opt-in-not-required"]
@@ -31,29 +21,37 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "tofu-controller-eks-${random_string.suffix.result}"
+  name   = "tofu-controller"
+  region = "ap-south-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    cluster_name    = local.name
+    For = "testing-purpose"
+    by  = "flux-iac/tofu-controller"
+  }
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
+################################################################################
+# VPC
+################################################################################
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
+  version = "~> 5.0"
 
-  name = "tofu-controller-vpc"
+  name = local.name
+  cidr = local.vpc_cidr
 
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = false
-  single_nat_gateway   = false
-  enable_dns_hostnames = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
@@ -62,63 +60,42 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
+
+  tags = local.tags
 }
- 
 
 
-module "eks" {
+module "eks_al2023" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "~> 20.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.30"
+  cluster_name    = "${local.name}-al2023"
+  cluster_version = "1.31"
 
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = false
-
-  
+  # EKS Addons
   cluster_addons = {
-    aws-ebs-csi-driver = {
-          enabled = false
-
-    }
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       instance_types = ["t3.small"]
 
-      min_size     = 1
-      max_size     = 1
+      min_size = 1
+      max_size = 1
+      # This value is ignored after the initial creation
+      # https://github.com/bryantbiggs/eks-desired-size-hack
       desired_size = 1
 
-      iam_role_additional_policies = {
-        "ViewOnlyAccessPolicy" = "arn:aws:iam::aws:policy/job-function/ViewOnlyAccess"
-      }
-    
-    # two = {
-    #   name = "node-group-2"
-
-    #   instance_types = ["t3.small"]
-
-    #   min_size     = 1
-    #   max_size     = 2
-    #   desired_size = 1
-    # }
-
+    }
   }
-  cluster_log_types = []
 
+  tags = local.tags
 }
-}
-
