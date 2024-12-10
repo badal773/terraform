1b8b5f21d8d6ba67553a4dd8c6171c110d0269e8
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2024-12-10 10:23:01 +0530

    debug-3

--- eks_cluster/main.tf
+++ eks_cluster/main.tf
@@ -36,7 +36,6 @@ module "vpc" {
   enable_nat_gateway   = false
   single_nat_gateway   = false
   enable_dns_hostnames = true
-  disableLogs= true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
