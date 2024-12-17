c3e2184910ded6f00a2acc4bf86c028d574f1f8d
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2024-12-17 11:48:23 +0530

    added support to read env variable for bucket name

--- s3_bucket_creation/main.tf
+++ s3_bucket_creation/main.tf
@@ -1,12 +1,18 @@
-resource "aws_s3_bucket" "bucket-create-from-tofu-controller" {
-  bucket = "bucket-create-from-tofu-controller"
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "bucket-by-badal773" # optional default value 
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name        #export TF_VAR_bucket_name="bucket-create-from-terrfrom-by-badal773"
 
   tags = {
-    Name        = "badal773"
-    Environment = "Dev"
+    name        = "badal773"
+    environment = "DevOps"
   }
 }
 
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
