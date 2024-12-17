720fbe85159b992359baeedb60414f1585e4c9fc
Badal Kumar Prusty <badalkumar@Badals-MacBook-Pro.local>
2024-12-17 11:59:04 +0530

    added support to read env variable for region name

--- s3_bucket_creation/main.tf
+++ s3_bucket_creation/main.tf
@@ -4,6 +4,12 @@ variable "bucket_name" {
   default     = "bucket-by-badal773" # optional default value 
 }
 
+variable "region_name" {
+  description = "The region for the S3 bucket"
+  type        = string
+  default     = "us-west-2" # optional default value 
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name        #export TF_VAR_bucket_name="bucket-create-from-terrfrom-by-badal773"
 
@@ -14,5 +20,5 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region_name    #export TF_VAR_region_name="ap-south-1"
 }
